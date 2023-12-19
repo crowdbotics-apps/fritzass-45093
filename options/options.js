67b005edd6c193938545ff274fbb6940774f1293
Crowdbotics <team@crowdbotics.com>
2023-12-19 08:07:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fritzass-45093",
+  url: "https://fritzass-45093.botics.co",
+  api: "https://fritzass-45093.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
